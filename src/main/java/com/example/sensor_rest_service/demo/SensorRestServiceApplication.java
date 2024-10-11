90312f3b950ad6f3d1d4323d67c5d9adc688b69e
Miklos Komlosy <2220859001@fh-burgenland.at>
2024-10-11 15:47:01 +0200

    add src and mvn

---
+++ src/main/java/com/example/sensor_rest_service/demo/SensorRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sensor_rest_service.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/sensor_rest_service/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.sensor_rest_service.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SensorRestServiceApplication.class);
+	}
+
+}
